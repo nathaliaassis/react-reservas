ceed588fec1afe756ac61d608a17c1592fdc811d
unknown <nathalia.assis@doutorhoje.com.br>
2020-04-17 18:47:31 -0300

    estrutura básica inicial

---
+++ src/pages/Reservas/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Reservas() {
+    return (
+        <div>
+            reservas
+        </div>
+    )
+}
---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Reservas from './pages/Reservas'
+
+export default function Routes() {
+    return (
+        <Switch>
+            <Route path='/' component={Home} exact/>
+            <Route path='/Reservas' component={Reservas} />
+        </Switch>
+    )
+}
