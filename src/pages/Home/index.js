073cd24688d8069773f6ee2fd012952b51eee501
unknown <nathalia.assis@doutorhoje.com.br>
2020-04-18 00:19:39 -0300

    configuração api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import api from '../../services/api';
 
 export default function Home() {
+    const [trips, setTrips] = useState([]);
+
+    useEffect(() => {
+        async function loadApi() {
+
+            const res = await api.get('trips');
+            setTrips(res.data);
+        }
+        loadApi();
+    }, []);
+
     return (
         <div>
             home
